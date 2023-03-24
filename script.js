c8b4b5c33353dfc334a4d6039cbf47c350da4b0f
Clupai8o0 <clupaio4@gmail.com>
2023-03-24 12:37:15 +0300

    init proj

---
+++ script.js
@@ -0,0 +1,44 @@
+let font;
+let graphic;
+
+function preload() {
+	font = loadFont("assets/spacegrotesk-medium.otf");
+}
+
+function setup() {
+	createCanvas(600, 600);
+
+	graphic = createGraphics(600, 600);
+
+	graphic.fill("#f3c043");
+	graphic.textFont(font);
+	graphic.textSize(800);
+	graphic.textAlign(CENTER, CENTER);
+	graphic.text("a", 300, 240);
+}
+
+function draw() {
+  background("#e84e3c");
+
+	const tileSize = 50;
+
+	for (let x = 0; x < 12; x++) {
+		for (let y = 0; y < 12; y++) {
+      const distortion = sin(frameCount * 0.05 + x * 0.5 + y * 0.3) * 50;
+
+			//source
+			const sx = x * tileSize + distortion;
+			const sy = y * tileSize;
+			const sw = tileSize;
+			const sh = tileSize;
+
+			//destination
+			const dx = x * tileSize;
+			const dy = y * tileSize;
+			const dw = tileSize;
+			const dh = tileSize;
+
+			image(graphic, dx, dy, dw, dh, sx, sy, sw, sh);
+		}
+	}
+}
